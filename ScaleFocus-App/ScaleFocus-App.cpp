b771233dff6da6bb614f4741322445b4aa29878c
bbkanev18 <56872423+bbkanev18@users.noreply.github.com>
2021-07-06 10:15:00 +0300

    Update ScaleFocus-App.cpp
    Test to insert value in db

--- ScaleFocus-App/ScaleFocus-App.cpp
+++ ScaleFocus-App/ScaleFocus-App.cpp
@@ -5,22 +5,29 @@
 
 int main() try
 {
-    auto const connstr = NANODBC_TEXT("Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=master;Trusted_Connection=yes;");
-    nanodbc::connection conn(connstr);
-    //nanodbc::execute(conn, NANODBC_TEXT("create table t (i int)"));
-    //nanodbc::execute(conn, NANODBC_TEXT("insert into t (1)"));
+	auto const connstr = NANODBC_TEXT("Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=Test;Trusted_Connection=yes;");
+	nanodbc::connection conn(connstr);
+	//nanodbc::execute(conn, NANODBC_TEXT("create table t (i int)"));
+	std::string user = "admin";
+	std::string password = "adminpass";
+	nanodbc::execute(conn, NANODBC_TEXT("insert into TestTable (UserName, Password) VALUES ('"+ user +"', '"+ password +"')"));
 
-    auto result = nanodbc::execute(conn, NANODBC_TEXT("SELECT 1 as One, N'Hello!'"));
-    while (result.next())
-    {
-        auto i = result.get<int>("One", -1);
-        auto s = result.get<nanodbc::string>(1, "");
-        std::cout << i << ", " << s << std::endl;
-    }
-    return EXIT_SUCCESS;
+	auto result = nanodbc::execute(conn, NANODBC_TEXT("SELECT Id, UserName, Password FROM TestTable"));
+	while (result.next())
+	{
+		auto id = result.get<int>(0);
+		auto i = result.get<nanodbc::string>(1);
+		auto s = result.get<nanodbc::string>(2);
+		//auto i = result.get<int>("One", -1);
+		//auto s = result.get<nanodbc::string>(1, "");
+		//std::cout << i << ", " << s << std::endl;
+		std::cout << id << ", " << i << ", " << s << "\n";
+	}
+	result = nanodbc::execute(conn, NANODBC_TEXT("DELETE FROM TestTable"));
+	return EXIT_SUCCESS;
 }
 catch (std::exception& e)
 {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
+	std::cerr << e.what() << std::endl;
+	return EXIT_FAILURE;
 }
